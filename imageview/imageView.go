92a3fe4c086311cc7669784e46f5a200c5dde777
argotnaut <161991025+argotnaut@users.noreply.github.com>
2026-04-04 13:39:15 -0400

    Added comments

--- imageview/imageView.go
+++ imageview/imageView.go
@@ -11,34 +11,39 @@ const (
 	DEFAULT_IMAGE_WIDTH  = 80
 )
 
+/*
+A model which handles the displaying/resizing of an image in ASCII characters
+*/
 type ImageViewModel struct {
-	imageBytes        []byte
-	stringifiedImage  string
-	currentDimensions tea.WindowSizeMsg
+	imageBytes        []byte            // The bytes of the original image
+	stringifiedImage  string            // The current ASCII representation of the image
+	currentDimensions tea.WindowSizeMsg // The dimensions within which to display the image
 }
 
+/*
+Returns a new ImageViewModel with the given image bytes
+*/
 func NewImageViewModelFromBytes(imageBytes []byte) (output ImageViewModel) {
 	output.imageBytes = imageBytes
 	output.RerenderImage(output.currentDimensions)
 	return
 }
 
+/*
+Returns an ImageViewModel with image bytes from an http request to the given URL
+*/
 func NewImageViewModelFromURL(imageURL string) ImageViewModel {
-	model := ImageViewModel{
-		currentDimensions: tea.WindowSizeMsg{
-			Width:  DEFAULT_IMAGE_WIDTH,
-			Height: DEFAULT_IMAGE_HEIGHT,
-		},
-	}
-	var err error
-	model.imageBytes, err = getImageBytesFromURL(imageURL)
+	newImageBytes, err := getImageBytesFromURL(imageURL)
 	if err != nil {
 		log.Fatalf("error getting the image bytes from the URL: %s - %v", imageURL, err)
 	}
-	model.RerenderImage(model.currentDimensions)
-	return model
+	return NewImageViewModelFromBytes(newImageBytes)
 }
 
+/*
+Rerenders the image's bytes in ASCII characters according to the given dimensions
+and sets the ImageViewModel's stringifiedImage to the result
+*/
 func (m *ImageViewModel) RerenderImage(newDimensions tea.WindowSizeMsg) {
 	widthHasChanged := m.currentDimensions.Width != newDimensions.Width
 	heightHasChanged := m.currentDimensions.Height != newDimensions.Height
@@ -56,7 +61,6 @@ func (m ImageViewModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	case tea.WindowSizeMsg:
 		m.RerenderImage(msg)
 		m.currentDimensions = msg
-
 	}
 	return m, nil
 }
